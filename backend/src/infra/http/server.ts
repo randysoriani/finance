266326d4ee532228c36ac05ec61005c3f5e47bc8
Randy Soriani <randysoriani@outlook.com>
2022-12-09 10:33:14 -0300

    chore: add usersRoutes to server

--- backend/src/infra/http/server.ts
+++ backend/src/infra/http/server.ts
@@ -1,3 +1,5 @@
 import express from 'express';
+import { usersRouter } from '../routes/usersRouter';
 
 export const server = express();
+server.use('/users', usersRouter);
--- backend/src/infra/routes/usersRouter.ts
+++ backend/src/infra/routes/usersRouter.ts
@@ -1,3 +1,7 @@
 import { Router } from 'express';
 
 export const usersRouter = Router();
+
+usersRouter.get('/', (req, res) => {
+    return res.status(501).json({message:'Not implemented'});
+})
